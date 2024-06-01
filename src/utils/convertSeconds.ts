690ada55a6370a3623138300ccb2c00d466c29a8
Kristijonas <kristi.sr@hotmail.com>
2024-06-01 07:53:53 +0300

    Updated convertSeconds util function to use Intl namespace

--- src/utils/convertSeconds.ts
+++ src/utils/convertSeconds.ts
@@ -1,15 +1,17 @@
 /**
  * Converts a number of seconds into a string formatted as "mm:ss".
  *
- * @param {number} secs - The number of seconds to convert.
+ * @param {number} seconds - The number of seconds to convert.
  * @returns {string} A string representing the input seconds in "mm:ss" format.
  * @example
  * const result = convertSeconds(150);
  * // Returns "02:30" for 150 seconds
  */
 
-export default function convertSeconds(secs: number): string {
-  const minutes = Math.floor(secs / 60)
-  const seconds = Math.floor(secs % 60)
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`
+export default function convertSeconds(seconds: number): string {
+  const minutesAndSeconds = new Intl.DateTimeFormat('en', {
+    minute: '2-digit',
+    second: '2-digit',
+  })
+  return `${minutesAndSeconds.format(new Date(seconds * 1000))}`
 }
